968f8dbe759579b11c6296c44774aa165db3d29d
Mtaha55 <38652177+Mtaha55@users.noreply.github.com>
2024-05-16 20:32:05 +0200

    Update MySQLDBConnection
    Update MySQLDBConnection

--- src/main/java/lexicon/se/Data/db/MySQLDBConnection.java
+++ src/main/java/lexicon/se/Data/db/MySQLDBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class MySQLDBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/todoit";
     private static final String USER = "root";
-    private static final String PASSWORD = "Hassan_45!";
+    private static final String PASSWORD = "1234";
 
     public static Connection getConnection() {
         Connection connection = null;
